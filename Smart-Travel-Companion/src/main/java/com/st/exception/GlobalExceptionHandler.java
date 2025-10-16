cca8751163b8bbc50d1d27428b71afce0d5e38e5
Priyant Prashant Nikhare <nikharepriyant@gmail.com>
2025-10-16 19:21:22 +0530

    Implement global exception handling

---
+++ Smart-Travel-Companion/src/main/java/com/st/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.st.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception ex) {
+    return new ResponseEntity<>("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
