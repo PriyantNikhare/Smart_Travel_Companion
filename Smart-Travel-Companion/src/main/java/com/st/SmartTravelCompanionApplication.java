5aeecb7014d5482c1bbeb2fa642ff24288b923d4
Priyant Prashant Nikhare <nikharepriyant@gmail.com>
2025-10-16 17:24:33 +0530

    Initialize SmartTravelCompanionApplication class

---
+++ Smart-Travel-Companion/src/main/java/com/st/SmartTravelCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTravelCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTravelCompanionApplication.class, args);
+	}
+
+}
